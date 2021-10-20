76ff41e7e208a42f2ab6313e6d88bc19ada476df
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2021-10-20 12:30:37 +0200

    Installing dependencies for RxSwift

---
+++ MVVM-Rx/Pods/Target Support Files/Pods-MVVM-Rx-MVVM-RxUITests/Pods-MVVM-Rx-MVVM-RxUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVVM_Rx_MVVM_RxUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MVVM_Rx_MVVM_RxUITests
+@end
---
+++ MVVM-Rx/Pods/Target Support Files/Pods-MVVM-RxTests/Pods-MVVM-RxTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MVVM_RxTests : NSObject
+@end
+@implementation PodsDummy_Pods_MVVM_RxTests
+@end
