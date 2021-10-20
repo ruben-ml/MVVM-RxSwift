66b7df512299a869e788b949a1e76479278ff61b
Rubén Muñoz López <ruben.munoz@ciberexperis.es>
2021-10-20 12:26:12 +0200

    Create project

---
+++ MVVM-Rx/MVVM-Rx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-Rx
+//
+//  Created by Rubén Muñoz López on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVM-Rx/MVVM-RxTests/MVVM_RxTests.swift
@@ -0,0 +1,33 @@
+//
+//  MVVM_RxTests.swift
+//  MVVM-RxTests
+//
+//  Created by Rubén Muñoz López on 20/10/21.
+//
+
+import XCTest
+@testable import MVVM_Rx
+
+class MVVM_RxTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
